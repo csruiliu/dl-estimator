4c5d01e2a230d4ca260718c9a5956f68aff0674f
csruiliu <rui.liu@gmx.com>
2021-02-06 22:22:42 -0600

    update ml_profiler

--- ml_profiler.py
+++ ml_profiler.py
@@ -2,6 +2,7 @@ import numpy as np
 import json
 import argparse
 import itertools
+from timeit import default_timer as timer
 import os
 
 import tensorflow as tf
@@ -25,6 +26,7 @@ from tensorflow_cifar.models.zfnet import ZFNet
 from tensorflow_cifar.tools.dataset_loader import load_cifar10_keras
 from tensorflow_cifar.tools.model_tools import train_model
 from tensorflow_cifar.tools.model_tools import evaluate_model
+
 
 '''
 def generate_ml_workload():
@@ -61,9 +63,49 @@ def generate_ml_workload():
     return conf_list
 '''
 
+if __name__ == "__main__":
 
-def profile_model():
-    print('Start Profiling {}'.format(model_name))
+    ###################################
+    # get all parameters
+    ###################################
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-p', '--profile', action='store', type=str, choices=['accuracy', 'steptime'],
+                        help='profile model accuracy or steptime')
+    parser.add_argument('-m', '--model', action='store', type=str, help='indicate training model')
+    parser.add_argument('-l', '--layer', action='store', type=int,
+                        help='indicate the layer for some models like resnet, densenet, resnext, vgg')
+    parser.add_argument('-g', '--group', action='store', type=int,
+                        help='indicate the conv group for some models like shufflenet')
+    parser.add_argument('-x', '--complex', action='store', type=float,
+                        help='indicate the model complex for some models like shufflenetv2')
+    parser.add_argument('-c', '--card', action='store', type=int, help='indicate the cardinality for resnext')
+
+    parser.add_argument('-b', '--batch', action='store', type=int, help='indicate the batch size for training.')
+    parser.add_argument('-r', '--learn', action='store', type=float, help='indicate the learning rate for training.')
+    parser.add_argument('-o', '--opt', action='store', type=str, help='indicate the optimizer for training.')
+    parser.add_argument('-e', '--epoch', action='store', type=int, help='indicate the training epoch.')
+
+    args = parser.parse_args()
+
+    profile_mode = args.profile
+    model_name = args.model
+    model_layer = args.layer
+    model_group = args.group
+    model_complex = args.complex
+    model_card = args.card
+
+    batch_size = args.batch
+    learn_rate = args.learn
+    optimizer = args.opt
+    epoch = args.epoch
+
+    num_output_classes = 10
+
+    ###################################
+    # prepare the model
+    ###################################
 
     if model_name == 'alexnet':
         model = AlexNet(num_output_classes)
@@ -102,7 +144,12 @@ def profile_model():
     label_ph = tf.placeholder(tf.int32, [None, 10])
     logit = model.build(feature_ph)
 
+    train_op = train_model(logit, label_ph, optimizer, learn_rate)
+    eval_op = evaluate_model(logit, label_ph)
+
+    #########################################33
     # count overall trainable parameters
+
     total_parameters = 0
     for variable in tf.trainable_variables():
         # shape is an array of tf.Dimension
@@ -112,11 +159,17 @@ def profile_model():
             variable_parameters *= dim.value
         total_parameters += variable_parameters
 
-    train_op = train_model(logit, label_ph, optimizer, learn_rate)
-    eval_op = evaluate_model(logit, label_ph)
+    ###############################
+    # load dataset
+    ###############################
 
-    # load cifar10 dataset
     train_feature, train_label, eval_feature, eval_label = load_cifar10_keras()
+
+    ###################################
+    # profile the model
+    ###################################
+
+    print('Start Profiling {}'.format(model_name))
 
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
@@ -128,9 +181,13 @@ def profile_model():
         num_batch = num_feature // batch_size
         rest_feature = num_feature - batch_size * num_batch
         acc_record_list = list()
+        time_record_list = list()
 
         # train the model
         for e in range(epoch):
+
+            start_time = timer()
+
             # shuffle the training data
             shf_indices = np.arange(num_feature)
             np.random.shuffle(shf_indices)
@@ -154,6 +211,10 @@ def profile_model():
             else:
                 print('no train feature left for this epoch')
 
+            end_time = timer()
+            step_time = end_time - start_time
+            time_record_list.append(step_time)
+
             print('start evaluation phrase')
             acc_sum = 0
             eval_batch_size = 50
@@ -171,70 +232,65 @@ def profile_model():
             print('evaluation accuracy:{}'.format(acc_avg))
             acc_record_list.append(acc_avg)
 
-    return acc_record_list, total_parameters
+    steptime_avg = sum(time_record_list) / len(time_record_list)
 
+    json_acc_path = '/home/ruiliu/Development/ml-estimator/mlbase/model_acc.json'
+    json_time_path = '/home/ruiliu/Development/ml-estimator/mlbase/model_time.json'
 
-if __name__ == "__main__":
-    # get parameters
-    parser = argparse.ArgumentParser()
+    # json_acc_path = '/home/ruiliu/Development/ml-estimator/mlbase/model_acc.json'
+    # json_time_path = '/home/ruiliu/Development/ml-estimator/mlbase/model_time.json'
 
-    parser.add_argument('-m', '--model', action='store', type=str, help='indicate training model')
-    parser.add_argument('-l', '--layer', action='store', type=int,
-                        help='indicate the layer for some models like resnet, densenet, resnext, vgg')
-    parser.add_argument('-g', '--group', action='store', type=int,
-                        help='indicate the conv group for some models like shufflenet')
-    parser.add_argument('-x', '--complex', action='store', type=float,
-                        help='indicate the model complex for some models like shufflenetv2')
-    parser.add_argument('-c', '--card', action='store', type=int, help='indicate the cardinality for resnext')
+    if profile_mode == 'accuracy':
+        if os.path.exists(json_acc_path):
+            with open(json_acc_path) as f:
+                model_json_list = json.load(f)
+        else:
+            model_json_list = list()
 
-    parser.add_argument('-b', '--batch', action='store', type=int, help='indicate the batch size for training.')
-    parser.add_argument('-r', '--learn', action='store', type=float, help='indicate the learning rate for training.')
-    parser.add_argument('-o', '--opt', action='store', type=str, help='indicate the optimizer for training.')
-    parser.add_argument('-e', '--epoch', action='store', type=int, help='indicate the training epoch.')
+        # create a dict for the conf
+        model_perf_dict = dict()
 
-    args = parser.parse_args()
+        model_perf_dict['model_name'] = model_name
+        model_perf_dict['num_parameters'] = total_parameters
+        # model_perf_dict['num_parameters'] = np.random.randint(low=1000000, high=5000000)
+        model_perf_dict['batch_size'] = batch_size
+        model_perf_dict['opt'] = optimizer
+        model_perf_dict['learn_rate'] = learn_rate
 
-    model_name = args.model
-    model_layer = args.layer
-    model_group = args.group
-    model_complex = args.complex
-    model_card = args.card
+        model_perf_dict['training_data'] = 'cifar'
+        model_perf_dict['classes'] = num_output_classes
 
-    batch_size = args.batch
-    learn_rate = args.learn
-    optimizer = args.opt
-    epoch = args.epoch
+        model_perf_dict['accuracy'] = acc_record_list
+        # model_perf_dict['accuracy'] = list(list(np.random.uniform(0, 1, 20)))
 
-    json_file_path = '/home/ruiliu/Development/ml-estimator/mlbase/model_acc.json'
-    # json_file_path = '/tank/local/ruiliu/ml-estimator/mlbase/model_acc.json'
+        model_json_list.append(model_perf_dict)
 
-    if os.path.exists(json_file_path):
-        with open(json_file_path) as f:
-            model_json_list = json.load(f)
+        with open(json_acc_path, 'w+') as f:
+            json.dump(model_json_list, f)
+
     else:
-        model_json_list = list()
+        if os.path.exists(json_time_path):
+            with open(json_time_path) as f:
+                model_json_list = json.load(f)
+        else:
+            model_json_list = list()
 
-    num_output_classes = 10
-    epoch = 20
+        # create a dict for the conf
+        model_perf_dict = dict()
 
-    # training the model
-    # acc_list, total_parameters = profile_model()
+        model_perf_dict['model_name'] = model_name
+        model_perf_dict['num_parameters'] = total_parameters
+        # model_perf_dict['num_parameters'] = np.random.randint(low=1000000, high=5000000)
+        model_perf_dict['batch_size'] = batch_size
+        model_perf_dict['opt'] = optimizer
+        model_perf_dict['learn_rate'] = learn_rate
 
-    # create a dict for the conf
-    model_perf_dict = dict()
+        model_perf_dict['training_data'] = 'cifar'
+        model_perf_dict['classes'] = num_output_classes
 
-    model_perf_dict['model_name'] = model_name
-    model_perf_dict['num_parameters'] = np.random.randint(low=1000000, high=5000000)
-    model_perf_dict['batch_size'] = batch_size
-    model_perf_dict['opt'] = optimizer
-    model_perf_dict['learn_rate'] = learn_rate
+        model_perf_dict['steptime'] = steptime_avg
 
-    model_perf_dict['training_data'] = 'cifar'
-    model_perf_dict['classes'] = num_output_classes
+        model_json_list.append(model_perf_dict)
 
-    model_perf_dict['accuracy'] = list(list(np.random.uniform(0, 1, 20)))
-
-    model_json_list.append(model_perf_dict)
-
-    with open(json_file_path, 'w+') as f:
-        json.dump(model_json_list, f)
+        with open(json_time_path, 'w+') as f:
+            json.dump(model_json_list, f)
